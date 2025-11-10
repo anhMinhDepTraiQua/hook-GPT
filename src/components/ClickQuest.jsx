ad27b25a0066da9a7d727733227a0da9d94b1eee
anhMinhDepTraiQua <ducanhbanche@gmail.com>
2025-11-10 21:40:07 +0700

    commit 2

--- src/components/ClickQuest.jsx
+++ src/components/ClickQuest.jsx
@@ -7,10 +7,20 @@ function ClickQuest() {
        console.log(`Ngươi vừa đạt tới cấp độ count = ${count}`);
        
     },[count]);
+    if (count === 10) {
+        alert("Chúc mừng! Bạn đã hoàn thành nhiệm vụ Click Quest!");
+        setCount(0);
+        const increaseButton = document.querySelector('.increase');
+        increaseButton.style.userSelect = 'none';
+    }
   return (
-    <button onClick={() => setCount(count + 1)}>
-        Clicked {count} times
+    <div>
+        <h1>Clicked {count} times</h1>
+    <button className='increase' onClick={() => setCount(count + 1)}>
+        increase
     </button>
+    <button onClick={() => setCount(0)}>Reset</button>
+    </div>
   )
 }
 
